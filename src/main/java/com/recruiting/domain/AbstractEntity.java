fec24b0a36840784ecd10a14fe9576936fbe3206
ginosian <marta.ginosian@gmail.com>
2017-09-06 18:31:10 +0400

    Adds db itentity key pooling.

--- src/main/java/com/recruiting/domain/AbstractEntity.java
+++ src/main/java/com/recruiting/domain/AbstractEntity.java
@@ -1,6 +1,7 @@
 package com.recruiting.domain;
 
 import com.recruiting.converter.LocalDateTimeAttributeConverter;
+import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
@@ -16,7 +17,28 @@ public abstract class AbstractEntity implements Serializable {
 
     // region Instance Fields
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GenericGenerator(
+            name = "sequenceGenerator",
+            strategy = "enhanced-sequence",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(
+                            name = "optimizer",
+                            value = "pooled-lo"
+                    ),
+                    @org.hibernate.annotations.Parameter(
+                            name = "initial_value",
+                            value = "1"
+                    ),
+                    @org.hibernate.annotations.Parameter(
+                            name = "increment_size",
+                            value = "5"
+                    )
+            }
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "sequenceGenerator"
+    )
     @Column(name = "id")
     protected Long id;
 
